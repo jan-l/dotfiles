47509716d2ae26c9bd510d4115d25943eb5a368e
Jan Lehtinen <me@janlehtinen.com>
2024-04-19 09:31:59 +0300

    Add nvim config

---
+++ private_dot_config/nvim/README.md
@@ -0,0 +1,4 @@
+# jan-l/nvim
+
+## Personal neovim configuration
+Based of kickstarter.nvim and LazyVim
---
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,56 @@
+return {
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+  -- Active indent guide and indent text objects. When you're browsing
+  -- code, this highlights the current level of indentation, and animates
+  -- the highlighting.
+  {
+    'echasnovski/mini.indentscope',
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = 'VeryLazy',
+    opts = {
+      -- symbol = "▏",
+      symbol = '│',
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'help',
+          'alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+}
