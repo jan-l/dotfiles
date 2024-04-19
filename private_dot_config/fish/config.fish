f92d5b7fafff2eeb2ac31b7bc2b56dba3e85b59e
Jan Lehtinen <me@janlehtinen.com>
2024-04-19 13:00:49 +0300

    Update fish configuration

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -5,14 +5,6 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 
-# nvm default node version
-# set --universal nvm_default_version v18.16.1
-
-#Artifactory tokens for ViewLinc
-set -x ARTIFACTORY_ID_TOKEN "cmVmdGtuOjAxOjE3NDM3MDgzMzY6MFJtRWVsWTRNYWVGYm1acWJ1WW5LdTl3UXVs"
-set -x ARTIFACTORY_USERNAME "ext-jan.lehtinen@vaisala.com"
-set -x ARTIFACTORY_AUTH_KEY (curl -s -u $ARTIFACTORY_USERNAME:$ARTIFACTORY_ID_TOKEN https://vaisala.jfrog.io/artifactory/api/npm/auth | egrep "_auth = [a-zA-Z0-9=]+" | cut -f3 -d ' ')
-
 fish_config theme choose "Catppuccin Mocha"
 fish_add_path $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin
 fish_add_path $HOME/.config/bin
